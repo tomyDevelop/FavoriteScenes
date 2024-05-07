85df5fc2eda6ca029a3d1ee462d5695e14d6f2a0
tomyDevelop <t05en865_d1s@docomo.ne.jp>
2024-05-07 11:03:53 +0900

    show_edit_profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,13 @@ class UsersController < ApplicationController
       redirect_to new_user_profile_path(@user.id), notice: t('users.form.msg.success')
     else
       # redirect_to new_user_path, alert: t('users.form.msg.failed')
-      flash.now[:alert] = t('users.form.msg.failed')
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @interests = Interest.order('sort_key')
   end
 
   private
